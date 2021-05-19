b999d3bc9ec19aef158e4ca4b44ccc37878dead7
szymonkacprowicz <szymonkrzysztof11@gmail.com>
2021-05-19 15:29:20 +0200

    first commit

---
+++ js/slickSettings.js
@@ -0,0 +1,30 @@
+$('.team-carousel').slick(
+
+    {
+        arrows: false,
+        autoplay: true,
+        mobileFirst: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+
+            {breakpoint: 767,
+                settings: { slidesToShow: 2}
+            
+            
+            },
+            {breakpoint: 992,
+                settings: { slidesToShow: 3}
+            
+            
+            },
+            {breakpoint: 1200,
+                settings: { slidesToShow: 4}
+            
+            
+            },
+
+        ]
+    }    
+  
+  );
